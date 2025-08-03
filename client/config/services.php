d4649cde6c966d6166ad49fe2fc20c575774023f
tuanphuc <phucnguyenphan93@gmail.com>
2025-08-04 05:00:57 +0700

    delete unused files

--- client/config/services.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('AUTH_SERVICE_URL', 'http://localhost:3001');
-define('APPOINTMENT_SERVICE_URL', 'http://localhost:3002');
-define('PATIENT_SERVICE_URL', 'http://localhost:3003');
-define('PRESCRIPTION_SERVICE_URL', 'http://localhost:3004');
-define('NOTIFICATION_SERVICE_URL', 'http://localhost:3005');
