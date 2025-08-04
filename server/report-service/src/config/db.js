2894f23bc10ff1b4c2426b485bd04d16ecd9371d
Kenko2102 <phuongrom0810@gmail.com>
2025-08-04 15:24:57 +0700

    Thêm report-service vào project

---
+++ server/report-service/src/config/db.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'report_service',
+});
+
+module.exports = connection;
